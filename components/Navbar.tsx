c76cb2f6f081d225c3087b6f8adfab9abb2f95fa
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-03 12:18:11 -0800

    Collapsable navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+'use client';
+import {useState} from 'react'
 import Link from 'next/link'
 
+import {HiMenu, HiX} from 'react-icons/hi'
+
 const Navbar = () => {
+
+  const [toggleIcon, setToggleIcon] = useState(false);
+  
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
-    <nav className='w-full'>
-        <div className='max-w-[1224px] mx-auto h-full flex items-center justify-between'>
-          <div></div>
-          <ul className='flex items-center justify-center space-x-6'>
-            <li><Link href='/'>Home</Link></li>
+    <nav className='w-full z-10'>
+        <div className='relative h-full flex items-center justify-between max-w-[1240px] m-auto p-6'>
+          <div>
+            <h1 className='text-4xl text-green-400'>JM.</h1>
+          </div>
+          <ul className='hidden md:flex flex-grow space-x-8  items-center justify-center h-full'>
+            <li className='active'><Link href='/'>Home</Link></li>
             <li><Link href='/'>About</Link></li>
-            <li><Link href='/'>Home</Link></li>
-            <li><Link href='/'>Home</Link></li>
+            <li><Link href='/'>Projects</Link></li>
+            <li><Link href='/'>Testimonials</Link></li>
+            <li><Link href='/'>Contact Me</Link></li>
           </ul>
+          <div className='md:hidden h-full' onClick={() =>setToggleIcon(!toggleIcon)}>
+            {toggleIcon ? <HiX size={33} className="cursor-pointer"/> : <HiMenu size={33} className="cursor-pointer"/>
+            }
+          </div>
+
+          {/* Mobile Menu */}
+          {
+            <div className={toggleMenu ? "" : ""}>
+              <ul className='md:hidden flex flex-grow space-x-8  items-center justify-center h-full '>
+                <li className='active'><Link href='/'>Home</Link></li>
+                <li><Link href='/'>About</Link></li>
+                <li><Link href='/'>Projects</Link></li>
+                <li><Link href='/'>Testimonials</Link></li>
+                <li><Link href='/'>Contact Me</Link></li>
+              </ul>
+            </div>
+        
+          }
         </div>
     </nav>
   )
